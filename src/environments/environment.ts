be48a5ee7bddf0b66008c20179c1e0c46238126c
mika481 <fradjbouain@gmail.com>
2026-03-24 21:54:15 +0100

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://omra-spring-production.up.railway.app',
   /** Clé API Google Cloud (Maps JavaScript API + Places) — https://console.cloud.google.com/google/maps-apis */
   googleMapsApiKey: '',
 };
